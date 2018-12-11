4ad22772b90073c0c623ccaee6a0c16114f78b28
liuzhangyi <44669013+liuzhangyi@users.noreply.github.com>
2018-12-11 15:33:31 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     '''====================
     Use your own agent here.'''
-    from game2048.agents import ExpectiMaxAgent as TestAgent
+    from game2048.agents import RandomAgent as TestAgent
     '''===================='''
 
     scores = []
